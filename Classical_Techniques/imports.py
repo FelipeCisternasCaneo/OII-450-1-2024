47994ef7845dfca941d8395c796102f536185cce
Felipe Cisternas Caneo <felipe.cisternas@pucv.cl>
2024-03-22 21:48:32 -0300

    Refresh

--- Classical_Techniques/imports.py
+++
@@ -1,5 +0,0 @@
-from Classical_Techniques.Codes.boruta import boruta
-from Classical_Techniques.Codes.minimal_redundancy_mazimum_relevance import MRMR
-from Classical_Techniques.Codes.mutual_information_classification import MIR
-from Classical_Techniques.Codes.recursive_feature_elimination import rfe
-from Classical_Techniques.Codes.sequential_feature_selection import SFS
--- MachineLearning/imports.py
+++
@@ -1,4 +0,0 @@
-from MachineLearning.Codes.KNN import KNN
-from MachineLearning.Codes.QLearning import QLearning
-from MachineLearning.Codes.RandomForest import RandomForest
-from MachineLearning.Codes.Xgboost import Xgboost
